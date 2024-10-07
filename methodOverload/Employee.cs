313ea96e162192acda36bd81dcd2bdacd69599ae
Nicholas Keyes <148286779+kingkashka@users.noreply.github.com>
2024-10-07 12:20:11 -0500

    Add project files.

---
+++ methodOverload/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodOverload
+{
+    public class Employee : Person
+    {
+        // Define the ID property
+        public int ID { get; set; }
+
+        // Constructor
+        public void sayID() 
+        {
+            Console.WriteLine($"ID: {this.ID}");                
+        }
+    }
+}
---
+++ methodOverload/Program.cs
@@ -0,0 +1,19 @@
+﻿using methodOverload;
+using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+     Employee employee = new Employee();
+        employee.firstName = "Sample";
+        employee.lastName = "Student";
+        employee.ID = 123456789;
+
+        // Call the instance methof on the employee object
+        employee.sayName();
+        employee.sayID();
+
+  
+    }
+}
